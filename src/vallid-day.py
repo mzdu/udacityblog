815ff9cbf684377e2e6c032baf88b4374ef52fa6
mzdu <dan.du.pub@gmail.com>
2013-04-12 00:19:37 -0400

    if day and day.isdigit():
    convet str to int   day = int(day)

---
+++ src/vallid-day.py
@@ -0,0 +1,14 @@
+def valid_day(day):
+#     if day>='1' and day<='31':
+#         return day
+
+    if day and day.isdigit():
+        day = int(day)
+        if day > 0 and day <=31:
+            return day
+
+print valid_day('15')
+# valid_day('0') => None    
+# valid_day('1') => 1
+# valid_day('15') => 15
+# valid_day('500') => None
