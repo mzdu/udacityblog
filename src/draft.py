a9ffdb36b7eb0b20f8b9aa1bb1d70e424143d0b6
mzdu <dan.du.pub@gmail.com>
2013-04-15 12:09:07 -0400

    add sqlite3 sample

--- src/draft.py
+++ src/draft.py
@@ -1,11 +1,16 @@
-errordict = {"erro1": "e1",
-             "erro2": "e2",
-             "erro3": "e3"
-             }
+list1 = [[1334018620.0, 'http://research.swtch.com/zip'], [1333900877.0, 'http://www.pushing-pixels.org/2011/11/04/about-those-vector-icons.html'], [1333939389.0, 'http://www.serpentine.com/blog/2012/04/08/github-is-making-me-feel-stupider/']]
 
-def write_form(dict1):
-    print 'Our error message: %(erro1)s is %s' % dict1
-    
+# 
+# i = 0
+# index1 = []
+# for i in range(len(list1)):
+#     dex1 = list1[i][0]
+#     index1.append(dex1)
+# 
+# index1.sort()
+# 
+# print index1
 
-write_form(errordict)  
-    
+list1.append([112,'22'])
+
+print list1
