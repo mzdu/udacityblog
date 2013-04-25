9d1c384869574e806e103e4a4383f091950a667d
mzdu <dan.du.pub@gmail.com>
2013-04-25 15:16:36 -0400

    finish basic template with AddBlog and Main

--- src/main.py
+++ src/main.py
@@ -1,75 +1,67 @@
+import os
 import webapp2
+import jinja2
 import logging
 
-form = '''
-<link href="/static/style.css" rel="stylesheet" type="text/css">
-<form method="POST">
-    What is your birthday? <br />
-    <label>Month
-        <input type="text" name="month" value=%(month)s />
-    </label>
-    <label>Date
-        <input type="text" name="date" value=%(day)s />
-    </label>
-    <label>Year
-    <input type="text" name="year" value=%(year)s />
-    </label>
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), 
+                               autoescape = True)
+class Article(db.Model):
+    #indicate title is String type
+    title = db.StringProperty(required = True)
+    #set it to current time
+    created = db.DateTimeProperty(auto_now_add = True)
     
-    <br />
-    <div style="color: red">%(error123)s</div>
-    <br />
+    body = db.TextProperty(required = True)
+
+class Handler(webapp2.RequestHandler):
     
-<input type = "submit">      
-</form>
-'''
-
-
-def valid_month(month):
-    if month and month.isdigit():
-        month = int(month)
-        if month >= 1 and month <= 12:
-            return month
-
-def valid_day(day):
-    if day and day.isdigit():
-        day = int(day)
-        if day >= 1 and day <= 31:
-            return day
-
-def valid_year(year):
-    if year and year.isdigit():
-        year = int(year)
-        if year >= 1900 and year <= 2020:
-            return year
-
-class ThanksHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write("Thanks! That is a valid submit!")
-
-class MainPage(webapp2.RequestHandler):
-    
-    def write_form(self, error="", month=" ", day=" ", year=" "):
-        self.response.out.write(form % {"error123": error,
-                                        "month": month,
-                                        "day": day,
-                                        "year": year})
-
-    def get(self):
-        self.write_form()
-        logging.info('get loaded')
-
-    def post(self):
-        user_month = valid_month(self.request.get('month'))
-        user_day = valid_month(self.request.get('date'))
-        user_year = valid_year(self.request.get('year'))
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
         
-        if not (user_month and user_day and user_year):
-            self.write_form("not valid")
-        else:
-            self.redirect('/thanks')
-
-
-
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))    
+    
+    def render_page(self, articles=""):
+#         arts = db.GqlQuery("select * from Art order by created DESC")
+#         self.render("index.html", title1=title, art1=art, error1=error, arts1=arts)
+        
+        self.render(page, articles=articles)
+        
+        
+class AddBlog(Handler):
+    def get(self):
+        self.render("addBlog.html")
+    
+    def post(self):
+        
+        
+        self.redirect("/")
+        
+class MainPage(Handler):
+    def get(self):
+        self.render("index.html")
+#         articles = db.GqlQuery("select * from Articles order by created DESC")
+#         self.render_page("index.html", articles = articles)         
+    def post(self):
+        self.write("this is a post main page")
+#         title2 = self.request.get("title1")
+#         art2 = self.request.get("art1")
+#         
+#         if title2 and art2:
+#             a = Article(title=title2,art=art2)
+#             a.put()
+#             self.redirect('/')
+#         else:
+#             error2 = "Sorry Error."
+#             
+#             self.render_page(title2, art2, error2)
+    
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/thanks', ThanksHandler)],
-                              debug=True)
+                               ('/addblog', AddBlog)], debug=True)
