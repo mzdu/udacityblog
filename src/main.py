7d6cacb5c5db1c7913d96d6bcf17a654c496922c
mzdu <dan.du.pub@gmail.com>
2013-04-30 21:23:29 -0400

    created fruit tracking sys.

--- src/main.py
+++ src/main.py
@@ -70,10 +70,27 @@ class SuccessHandler(Handler):
         
 class MainPage(Handler):
     def get(self):
-
         articles = db.GqlQuery("select * from Article order by created DESC")
-        
         self.render("index.html", Articles = articles)
+        
+        
+        #setup cookies
+        visits = self.request.cookies.get('visits', '0')
+        visits = int(visits) + 1
+        self.response.headers.add_header('Set-cookie', 'visits=%s' % visits)
+        self.write("you have been here %s times." % visits)
+        # CRC32 - checksums, fast to prevent cookies from modification
+        # MD5 - fast, secure kind of, for limited input
+        # SHA1 - secure
+        # SHA256 - pretty good, but slower
+        '''
+        import hashlib
+        hashlib.md5("hello").hexdigit()
+        ==> '23u2839280380023...'
+        '''
+        
+        
+        
         
 #         self.render_page("index.html", articles = articles)         
     def post(self):
