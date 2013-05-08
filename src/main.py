81f3b97c8eab8966fa2f8eb66526a2f23109d334
mzdu <dan.du.pub@gmail.com>
2013-05-08 15:35:36 -0400

    coommit

--- src/main.py
+++ src/main.py
@@ -26,6 +26,9 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
         
+        
+        
+        
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
